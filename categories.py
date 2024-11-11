b5359bd3f2372572bb09a973ff33ecb31736bf57
Everson Soares <eversonv4@gmail.com>
2024-11-11 01:47:58 -0300

    initial commit

---
+++ categories.py
@@ -0,0 +1,22 @@
+categories = [
+  {
+    "category": "Cachorro",
+    "url": "https://www.petz.com.br/cachorro/brinquedos",
+    "id": 1
+  },
+  {
+    "category": "Gato",
+    "url": "https://www.petz.com.br/gato/arranhadores-e-brinquedos",
+    "id": 2
+  },
+  {
+    "category": "Racoes",
+    "url": "https://www.petz.com.br/cachorro/racao",
+    "id": 3
+  },
+  {
+    "category": "Higiene",
+    "url": "https://www.petz.com.br/cachorro/beleza-e-limpeza",
+    "id": 4
+  }
+]
